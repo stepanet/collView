b0a6c94c486b5c64ec2232d5c7c083274484d458
stepanet <stepanet@gmail.com>
2019-04-11 22:42:03 +0300

    Initial Commit

---
+++ collView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  collView
+//
+//  Created by Jack Sp@rroW on 11/04/2019.
+//  Copyright © 2019 Jack Sp@rroW. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
